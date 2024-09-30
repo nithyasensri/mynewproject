e093424dc65d8f6360da269d3de3816183c0c25d
Manoj Kumar <manoj@Manojs-MacBook-Air.local>
2024-09-30 21:39:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" style={{"display":"flex","background":"pink"}}>
+       <h3>Developer</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
